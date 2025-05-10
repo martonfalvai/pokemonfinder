95fb3efc77b9696e8f9cfb5d468c59b48bbc29b4
Martonfalvai Manassé <manas@Martonfalvai-MacBook-Air.local>
2025-05-10 09:31:14 +0200

    init projekt

---
+++ index.js
@@ -0,0 +1,9 @@
+const components = {
+    button: 'search-button',
+    input: 'search-input',
+    conatainer: 'pokemon-container'
+}
+
+document.getElementById(components.button).addEventListener('click',()=>{
+    alert("Keresünk!")
+})
