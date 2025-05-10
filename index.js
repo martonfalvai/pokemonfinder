26fd0bfde7f26047c9e59db86ad270d3441ed818
Martonfalvai Manassé <manas@Martonfalvai-MacBook-Air.local>
2025-05-10 10:24:19 +0200

    v1

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
+const OPTIONS = {
+    api: 'https://pokeapi.co/api/v2/pokemon/ditto'
+}
+
 const components = {
     button: 'search-button',
     input: 'search-input',
-    conatainer: 'pokemon-container'
+    container: 'pokemon-container'
 }
 
-document.getElementById(components.button).addEventListener('click',()=>{
+document.getElementById(components.button).addEventListener('click', async ()=>{
+    //handlePokemonRequest();
+    const data = await getPokemonByName(document.getElementById(components.input).value)
+    createPokemonImage(data)
     alert("Keresünk!")
 })
+
+
+async function getPokemonByName(pokemonName){
+   const response = await fetch(OPTIONS.api)
+   const data = await response.json()
+   return data;
+   
+}
+
+function createPokemonImage(data){
+    const pokemonImg = data.sprites.front_default
+    const img = document.createElement('img')
+    img.src = pokemonImg
+    document.getElementById(components.container).append(img)
+}
+
+//gitre fel
+// refactor, rövidít
+//input üres - letiltás
+//megjelenítés előtt előző törlése
